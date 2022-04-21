679b9263e1ca3004d602a2abad28d2fbec0e2599
geo-js <93865236+geo-js@users.noreply.github.com>
2022-04-21 18:51:04 +0200

    Update konversio-subnav.js

--- konversio-subnav.js
+++ konversio-subnav.js
@@ -18,16 +18,25 @@ document.addEventListener('DOMContentLoaded', function(e) {
 
 
         if(currentScroll >= $( window ).height()) { 
-           document.getElementById("sub-menu-link-text-1").classList.remove("is-startseite");
+        document.getElementById("sub-menu-link-text-1").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-2").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-3").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-4").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-5").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-6").classList.remove("is-startseite");
+	document.getElementById("subnav-bg-filled").style.display = "none";
+    	document.getElementById("subnav-bg-outline").style.display = "block"; 
         }
      
         if(currentScroll <= $( window ).height()) {  
-            
+        document.getElementById("sub-menu-link-text-1").classList.add("is-startseite");
+    	document.getElementById("sub-menu-link-text-2").classList.add("is-startseite");
+    	document.getElementById("sub-menu-link-text-3").classList.add("is-startseite");
+    	document.getElementById("sub-menu-link-text-4").classList.add("is-startseite");
+    	document.getElementById("sub-menu-link-text-5").classList.add("is-startseite");
+    	document.getElementById("sub-menu-link-text-6").classList.add("is-startseite");
+   	document.getElementById("subnav-bg-filled").style.display = "block";
+    	document.getElementById("subnav-bg-outline").style.display = "none";  
         }
      
     })
