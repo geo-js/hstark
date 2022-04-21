8c8afb79f31af258522876f3388d91c430b0a27b
geo-js <93865236+geo-js@users.noreply.github.com>
2022-04-21 19:02:23 +0200

    Update and rename konversio-subnav-2.js to konversio-subnav-3.js

--- konversio-subnav-3.js
+++ konversio-subnav-3.js
@@ -14,10 +14,11 @@
 // If Scrolling
 document.addEventListener('DOMContentLoaded', function(e) {
     document.addEventListener('scroll', function(e) {
-        let currentScroll = window.scrollY;
+	let currentScroll = window.scrollY;
+        /* let currentScroll = window.scrollY; */ // 100vh
 
 
-        if(currentScroll >= $( window ).height()) { 
+        if(currentScroll >= 200) { 
         document.getElementById("sub-menu-link-text-1").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-2").classList.remove("is-startseite");
     	document.getElementById("sub-menu-link-text-3").classList.remove("is-startseite");
@@ -28,7 +29,7 @@ document.addEventListener('DOMContentLoaded', function(e) {
     	document.getElementById("subnav-bg-outline").style.display = "none"; 
         }
      
-        if(currentScroll <= $( window ).height()) {  
+        if(currentScroll <= 200) {  
         document.getElementById("sub-menu-link-text-1").classList.add("is-startseite");
     	document.getElementById("sub-menu-link-text-2").classList.add("is-startseite");
     	document.getElementById("sub-menu-link-text-3").classList.add("is-startseite");
